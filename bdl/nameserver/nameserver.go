c49491c7f0d6ba3c0e2b53d85765c603e7d3e52e
j-haj <jeff.hajewski@gmail.com>
2019-07-09 20:09:35 -0500

    Finishes the nameserver RPC service implementation.

--- bdl/nameserver/nameserver.go
+++ bdl/nameserver/nameserver.go
@@ -3,7 +3,9 @@ package nameserver
 import (
 	"context"
 	"flag"
+	"math/rand"
 	"sync"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -14,48 +16,115 @@ import (
 var (
 	nameserverAddress = flag.String("nameserver_address", "localhost:10000",
 		"Address used by the nameserver.")
-	debug = flag.Bool("debug", false, "Enable debug logging.")
-	logFile = flag.String("log_file", "", "Path to file used for logging.")
-	timeout = flat.Int("timeout", 5, "Default timeout in seconds for RPCs.")
+	debug             = flag.Bool("debug", false, "Enable debug logging.")
+	logFile           = flag.String("log_file", "", "Path to file used for logging.")
+	timeout           = flag.Int("timeout", 5, "Default timeout in seconds for RPCs.")
+	connectionTimeout = flag.Float64("connection_timeout", 120, "Time to wait in seconds befor removing a broker.")
 )
 
-
 type broker struct {
-	address string
-	client pbNS.BrokerNameServiceClient
+	address  string
+	location string
+	client   pbNS.BrokerNameServiceClient
 }
 
 type brokerID int64
 
 type nameserver struct {
 
-	// Stores when the last heartbeat was received for each 
-	heartbeats map[brokerID]float64
-	
+	// Stores when the last heartbeat was received for each
+	heartbeats map[brokerID]time.Time
+
 	// Stores the brokers known by the nameserver
-	brokers map[brokerID]broker
+	brokers map[brokerID]*broker
+
+	locations map[string]map[brokerID]bool
 
 	nextBrokerID brokerID
 
 	mu sync.Mutex
 }
 
-func (ns *nameserver) Register(ctx context.Context, req *pbNS.RegistrationRequest) (pbNS.RegistrationResponse, error) {
+// Register a broker with the nameserver
+func (ns *nameserver) Register(ctx context.Context, req *pbNS.RegistrationRequest) (*pbNS.RegistrationResponse, error) {
+	address := req.GetAddress()
+	location := req.GetLocation()
+	types := req.GetTypes()
 	log.WithFields(log.Fields{
-		"address": req.GetAddress(),
-		"types": req.GetTypes(),
+		"address":  address,
+		"location": location,
+		"types":    types,
 	}).Debug("Recevied broker registration request.")
 	ns.mu.Lock()
 	defer ns.mu.Unlock()
 
 	// Create connection
-	conn, err := grpc.Dial(req.GetAddress(), grpc.WithInsecure())
+	// TODO: I'm not sure we actually need this connection...
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.WithFields(log.Fields{
-			"address": req.GetAddress(),
+			"address": address,
 		}).Error("Failed to establish connection.")
+		return nil, fmt.Errorf("failed to connect to %s - %v", req.GetAddress(), err)
 	}
+
 	// Create broker nameservice client
+	id := ns.nextBrokerID
+	ns.brokers[id] = &broker{address, location, req.NewBrokerServiceClient(conn)}
+	if ns.locations[location] == nil {
+		ns.locations[location] = make(map[brokerID]bool)
+	}
+	ns.locations[location][id] = true
+	ns.nextBrokerID++
+	return &pbNS.RegistrationResponse{Id: id}, nil
+}
+
+// Receive a heartbeat from a broker.
+func (ns *nameserver) Heartbeat(ctx context.Context, req *pbNS.HeartbeatRequest) (*pbNS.HeartbeatResponse, error) {
+	rcvd := time.Now()
+	id := brokerID(req.GetId())
+	log.WithFields(log.Fields{
+		"id":   id,
+		"time": rcvd,
+	}).Debug("Received heartbeat.")
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	if p, ok := ns.heartbeats[id]; !ok {
+		// Handle the case where we don't have a heartbeat record - this occurs when a broker
+		// has been dropped
+		return &pbNS.HeartbeatResponse{Reregister: true}, nil
+	}
+	ns.heartbeats[id] = rcvd
+	return &pbNS.HeartbeatResponse{Reregister: false}, nil
+}
+
+func (ns *nameserver) RequestBroker(ctx context.Context, req *pbNS.BrokerRequest) (*pbNS.BrokerInfo, error) {
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	location := req.GetLocation()
+	n := len(ns.locations[location])
+	idx := brokerID(rand.Intn(n))
+	return &pbNS.BrokerInfo{Address: ns.brokers[idx].address}, nil
+}
+
+func (ns *nameserver) checkHeartbeats() {
+	ns.mu.Lock()
+	defer ns.mu.Unlock()
+	dead := &[]int{}
+	// Find dead connections
+	for k, v := range ns.heartbeats {
+		if time.Since(v).Seconds() > connectionTimeout {
+			dead = append(dead, k)
+		}
+	}
+
+	// Remove dead connections
+	for _, k := range dead {
+		l := ns.brokers[k].location
+		delete(ns.heartbeats, k)
+		delete(ns.brokers, k)
+		delete(ns.locations[l], k)
+	}
 }
 
 func main() {
