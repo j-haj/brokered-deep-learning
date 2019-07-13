f0155df4715cf6c1975f8c05c9a990a6b72363e9
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-07-13 14:02:27 -0500

    Update proto gen script to build new protos.

--- bdl/proto_gen.sh
+++ bdl/proto_gen.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+protoc -I. task/task.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I. broker_comm/broker_comm.proto --go_out=plugins=grpc,paths=source_relative:.
 protoc -I. nameservice/nameservice.proto --go_out=plugins=grpc,paths=source_relative:.
 protoc -I. heartbeat/heartbeat.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I. result/result.proto --go_out=plugins=grpc,paths=source_relative:.
+
