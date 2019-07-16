38e7d1e84d304fc2706a8e448c84b97455032f64
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-07-16 16:19:40 -0500

    Update proto gen script for task service rename.

--- bdl/proto_gen.sh
+++ bdl/proto_gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-protoc -I. task/task.proto --go_out=plugins=grpc,paths=source_relative:.
+protoc -I. task_service/task_service.proto --go_out=plugins=grpc,paths=source_relative:.
 protoc -I. broker_comm/broker_comm.proto --go_out=plugins=grpc,paths=source_relative:.
 protoc -I. nameservice/nameservice.proto --go_out=plugins=grpc,paths=source_relative:.
 protoc -I. heartbeat/heartbeat.proto --go_out=plugins=grpc,paths=source_relative:.
