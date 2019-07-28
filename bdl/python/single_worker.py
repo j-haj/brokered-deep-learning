38e6f8bc825890aa9bb1037db9dae84dfab662c1
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-07-28 16:54:36 -0500

    Creates single worker runner.

---
+++ bdl/python/single_worker.py
@@ -0,0 +1,84 @@
+import argparse
+import logging
+import time
+
+import numpy as np
+
+from model_runner.model_runner import EvoBuilder
+from nn.data import mnist_loaders, fashion_mnist_loaders, cifar10_loaders, Dataset
+from nn.classification import SimpleNN
+from nn.genotype import Population
+from nn.layer import LayerType, layers_from_string
+from nn.network_task import NetworkTask
+
+_DATASETS = {"fashion_mnist": Dataset.FASHION_MNIST,
+             "mnist": Dataset.MNIST,
+             "cifar10": Dataset.CIFAR10}
+
+def append_to_file(accuracies, path="single_model_results.csv"):
+    with open(path, "a") as f:
+        for r in accuracies:
+            f.write("{},{:.4f},{:.8f},{}\n".format(r[0], r[1], r[2], r[3]))
+
+def run(population, dataset, n_generations=100):
+    start = time.time()
+    accuracies = []
+    initial_size = len(population.population)
+    for generation in range(n_generations):
+        logging.debug("Generation %d" % (generation+1))
+        # Evolve population
+        logging.debug("Generating population offspring.")
+        population.generate_offspring()
+        
+        # Evaluate population
+        logging.debug("Evaluating population.")
+        for g in population:
+            if g.is_evaluated():
+                accuracies.append([generation, time.time() - start,
+                                   g.fitness(), g.model()])
+            m = NetworkTask(g.model().to_string(), dataset, 128, n_epochs=2) 
+            r = m.run()
+            accuracies.append([generation, time.time() - start,
+                               r.accuracy(), g.model()])
+
+        keepers = []
+        while len(keepers) < initial_size:
+            g1, g2 = np.random.choice(population.offspring, size=2)
+            if g1.fitness() > g2.fitness():
+                keepers.append(g1)
+            else:
+                keepers.append(g2)
+        population.update_population(keepers)
+        logging.debug("Appending accuracies to results file.")
+        append_to_file(accuracies)
+        accuracies = []
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="fashion_mnist",
+                        help=("Dataset to use. Can be one of mnist, "
+                              "fashion_mnist (default), or cifar10."))
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode.")
+    parser.add_argument("--population_size", type=int, default=10, help="Population size.")
+    parser.add_argument("--max_layer_count", type=int, default=3,
+                        help="Max number of layers per module.")
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    fmt_str = "[%(levelname)s][%(filename)s][%(funcName)s][%(lineno)d][%(message)s]"
+    if args.debug:
+        logging.basicConfig(format=fmt_str, level=logging.DEBUG)
+        logging.debug("Using DEBUG log level.")
+    else:
+        logging.basicConfig(format=fmt_str, level=logging.INFO)
+
+
+    population = Population(args.population_size,
+                            EvoBuilder(args.max_layer_count))
+    run(population, _DATASETS[args.dataset])
+    pass
+
+if __name__ == "__main__":
+    main()
