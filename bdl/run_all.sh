9152524b6441cab88974a7c5a83500a172e67117
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-08-03 20:02:16 +0000

    Updates run all for results naming.

--- bdl/run_all.sh
+++ bdl/run_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Set out files
-N=2
+N=4
 NS_OUT="ns$N.out"
 B_OUT="broker$N.out"
 M_OUT="model$N.out"
+RESULTS_PATH="results_$N.csv"
+printf "Saving results to $RESULTS_PATH"
 
 # Server addresses
-NAMESERVER_ADDR="localhost:10001"
-BROKER_ADDR="localhost:10002"
-MODEL_ADDR="localhost:20002"
+NAMESERVER_ADDR="localhost:10003"
+BROKER_ADDR="localhost:10004"
+MODEL_ADDR="localhost:20003"
 
 # Start nameserver
 nohup go run nameserver/nameserver.go -nameserver_address=$NAMESERVER_ADDR > $NS_OUT &
@@ -29,27 +31,27 @@ sleep .5
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=0 > w1-$N.out &
+      --cuda_device_id=2 > w1-$N.out &
 W1_PID=$!
 
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=1 > w2-$N.out &
+      --cuda_device_id=3 > w2-$N.out &
 W2_PID=$!
 
-#nohup python3 python/worker.py \
-#      --debug \
-#      --broker_address=$BROKER_ADDR \
-#      --cuda_device_id=4 > w3-$N.out &
-#W3_PID=$!
-#
-#nohup python3 python/worker.py \
-#      --debug \
-#      --broker_address=$BROKER_ADDR \
-#      --cuda_device_id=5 > w4-$N.out &
-#W4_PID=$!
-#
+nohup python3 python/worker.py \
+      --debug \
+      --broker_address=$BROKER_ADDR \
+      --cuda_device_id=4 > w3-$N.out &
+W3_PID=$!
+
+nohup python3 python/worker.py \
+      --debug \
+      --broker_address=$BROKER_ADDR \
+      --cuda_device_id=5 > w4-$N.out &
+W4_PID=$!
+
 echo "$N workers started. Starting model."
 
 # Start model
@@ -61,7 +63,7 @@ nohup python3 python/model.py \
       --population_size=10 \
       --max_layer_size=5 \
       --n_modules=3 \
-      --result_path="results_$Nworkers.csv" \
+      --result_path=$RESULTS_PATH \
       --debug > $M_OUT &
 
 MODEL_PID=$!
