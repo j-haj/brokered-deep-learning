fa0446fad489c7b049901de4f1aa59b0628de87f
Jeff Hajewski <jeff.hajewski@gmail.com>
2019-08-04 00:09:37 +0000

    Adds 2, 4, and 8 results.

--- bdl/run_all.sh
+++ bdl/run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Set out files
-N=2
+N=8
 NS_OUT="ns$N.out"
 B_OUT="broker$N.out"
 M_OUT="model$N.out"
 RESULTS_PATH="results_$N.csv"
-printf "Saving results to $RESULTS_PATH"
+printf "Saving results to $RESULTS_PATH\n"
 
 # Server addresses
 NAMESERVER_ADDR="localhost:10003"
@@ -31,58 +31,53 @@ sleep .5
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=2 > w1-$N.out &
+      --cuda_device_id=0 > w1-$N.out &
 W1_PID=$!
 
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=3 > w2-$N.out &
+      --cuda_device_id=1 > w2-$N.out &
 W2_PID=$!
 
-if [$N -gt 2]
-then
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=4 > w3-$N.out &
+      --cuda_device_id=2 > w3-$N.out &
 W3_PID=$!
 
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=5 > w4-$N.out &
+      --cuda_device_id=3 > w4-$N.out &
 W4_PID=$!
-fi
 
-if [$N -gt 4]
-then
+
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=2 > w1-$N.out &
+      --cuda_device_id=4 > w5-$N.out &
 W5_PID=$!
 
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=5 > w2-$N.out &
+      --cuda_device_id=5 > w6-$N.out &
 W6_PID=$!
 
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=6 > w3-$N.out &
+      --cuda_device_id=6 > w7-$N.out &
 W7_PID=$!
 
 nohup python3 python/worker.py \
       --debug \
       --broker_address=$BROKER_ADDR \
-      --cuda_device_id=7 > w4-$N.out &
+      --cuda_device_id=7 > w8-$N.out &
 W8_PID=$!
-fi
 
-echo "$N workers started. Starting model."
+printf "$N workers started. Starting model.\n"
 
 # Start model
 nohup python3 python/model.py \
@@ -91,13 +86,13 @@ nohup python3 python/model.py \
       --dataset=cifar10 \
       --epochs=10 \
       --population_size=10 \
-      --max_layer_size=5 \
+      --max_layer_size=4 \
       --n_modules=3 \
       --result_path=$RESULTS_PATH \
       --debug > $M_OUT &
 
 MODEL_PID=$!
 
-echo "PIDs:"
-printf "\tNS: $NS_PID\n\tBROKER: $BROKER_PID\n\tMODEL: $MODEL_PID"
+printf "\n\nPIDs:\n"
+printf "\tNS: $NS_PID\n\tBROKER: $BROKER_PID\n\tMODEL: $MODEL_PID\n\n"
 
